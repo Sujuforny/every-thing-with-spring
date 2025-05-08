0a9d68cb257928b5345364d1cedccc593296acc1
seangpengny <pengny2002@gmail.com>
2025-05-08 10:42:19 +0700

    spring simple with Scheduler

--- src/main/java/com/suju/every_thing_with_spring/EveryThingWithSpringApplication.java
+++ src/main/java/com/suju/every_thing_with_spring/EveryThingWithSpringApplication.java
@@ -2,8 +2,10 @@ package com.suju.every_thing_with_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EveryThingWithSpringApplication {
 
 	public static void main(String[] args) {
